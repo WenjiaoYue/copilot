e48c74977921a82dfd5fb8571f6bcf72851de5ea
Deng, Yuxuan <yuxuan.deng@intel.com>
2023-12-04 14:17:07 +0800

    feat: refactor config url

--- src/utils/fetchContent.ts
+++ src/utils/fetchContent.ts
@@ -1,10 +1,10 @@
 import fetch from "node-fetch";
-import * as https from 'https';
+import { URLConfig, mode } from "../config";
 
-const URL = 'https://talkingphoto.eglb.intel.com/v1/code_generation';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 export async function fetchTextContent(keyword: string): Promise<string> {
-    const rs = await fetch(URL, {
+    // const url = mode.value ? URLConfig.highQuality : URLConfig.***
+    const rs = await fetch(URLConfig.highQuality, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
