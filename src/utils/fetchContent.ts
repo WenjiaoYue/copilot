418096ca53468d5a202e7d4548dd91b5cd2c2128
Yue, Wenjiao <wenjiao.yue@intel.com>
2023-12-13 14:32:04 +0800

    test mock api

--- src/utils/fetchContent.ts
+++ src/utils/fetchContent.ts
@@ -19,3 +19,19 @@ export async function fetchTextContent(keyword: string): Promise<string> {
     const rs_json = await rs.json();
     return rs_json.response;
 }
+
+
+export async function fetchTest(): Promise<string> {
+    const url = "https://4f30efea-8c4a-494a-af20-268e77c787c5.mock.pstmn.io/api/test";
+        
+    const rs = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });    
+    const rs_json = await rs.json();
+    console.log('12/13 --', rs_json.text);
+    
+    return rs_json.text;
+}
