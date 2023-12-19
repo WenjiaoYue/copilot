80d21cc5cf7756f935e337633c77cbc53e0958ea
Yue, Wenjiao <wenjiao.yue@intel.com>
2023-12-19 19:08:13 +0800

    update input height

--- src/utils/chatgpt.ts
+++ src/utils/chatgpt.ts
@@ -117,9 +117,7 @@ export async function chatgptSendMessage(this: any, text: string, opts: ChatGPTS
   }
 
   const url = getConfig().settings.codeChatUrl;
-  console.log('codeChatUrl', url);
   
-  // const url = "https://64d7da36-ccd1-4b5e-93ac-55e9ceaa8b0e.mock.pstmn.io/v1/code_generation"
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   const headers = {
     Accept: "text/event-stream",
